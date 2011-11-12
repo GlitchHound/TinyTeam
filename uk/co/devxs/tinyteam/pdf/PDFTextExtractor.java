03ac39c2e34836de251133d2481b9dd5457c7439
Your Name <robert.matear@googlemail.com>
2011-11-12 16:58:24 +0000

    PDFExtractor Update

--- uk/co/devxs/tinyteam/pdf/PDFTextExtractor.java
+++ uk/co/devxs/tinyteam/pdf/PDFTextExtractor.java
@@ -2,8 +2,20 @@ package uk.co.devxs.tinyteam.pdf;
 /** Extract the text from a PDF document **/
 
 import org.apache.pdfbox.util.PDFTextStripper;
+import java.io.*;
+
 
 public class PDFTextExtractor
 {
-   
+	private PDFTextStripper document;
+	   
+	public PDFTextExtractor(String filename)
+	{  
+		File pdfFile;
+		
+		try{
+			document = new PDFTextStripper();
+			pdfFile = new File(filename);
+		} catch(IOException e) {};
+	}
 }
