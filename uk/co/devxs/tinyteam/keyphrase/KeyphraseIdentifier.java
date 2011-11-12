90079cd923688890d94c4a55b7ffd8e59eed0e91
Charlie Brensinger <c.p.brensinger@student.liv.ac.uk>
2011-11-12 21:53:59 +0000

    LingPipe (Key phrase recognition) beginnings.

---
+++ uk/co/devxs/tinyteam/keyphrase/KeyphraseIdentifier.java
@@ -0,0 +1,20 @@
+/* Class KeyphraseIdentifier
+ * 
+ * Identifies key phrases in an input string.
+ *
+ *
+*/
+import 
+
+
+public class KeyphraseIdentifer
+{
+
+	public KeyphraseIdentifier(String inputString)
+	{
+
+		
+
+	}
+
+}
